c061c6c7d6ad80515d88aa12dbd28071909710c4
Cannon Lock <lockcannon@hotmail.com>
2020-10-15 18:24:49 -0500

    Quick Change

--- src/data/projectData.jsx
+++ src/data/projectData.jsx
@@ -3,13 +3,29 @@ import Moment from "moment";
 const solarData = [
   {
     id: 0,
-    title: "title1",
+    title: "This is Project :) (smile)",
     date: new Moment("2019-10-31"),
-    imgUrl: "",
+    imgUrl: "src\static\images\sun.jpg",
     summary:
-      "drbghdrb dfrtbhdrsb  dfgvdersf dersgvesd df grsdgb gergr egr esdrgddf gdercv gdedr",
+      "This is a test summary for the Project.",
     body:
-      "body1 sdlgvj esb gjmve  bfhd frbnob desklbg vdrlbd fkmbd fkmdbfk mbcdfljklmnjmnklmklddrl"
+      `A man named Billy Colman rescues a redbone hound under attack by neighborhood dogs. He takes it home with him so that its wounds can heal. In light of this event, he has a flashback to when he was a ten-year-old boy living in the Ozark Mountains.
+
+      The book is Wilson Rawls own story with Billy Colman as the fictional character who wants nothing more than a pair of Redbone Coonhounds for hunting. After seeing a magazine ad for coonhounds, Billy spends the next two years working odd jobs to earn the $50 he needs to buy two puppies. Billy's dogs are delivered to Tahlequah, over 20 miles away. Billy decides to walk the distance. As he returns with the dogs, he sees a heart carved on a tree with the names "Dan + Ann" and decides to name the puppies Little Ann and Old Dan. With his grandfather's help, Billy teaches his dogs to hunt. Both dogs are very loyal to each other and to Billy.
+      
+      The first night of hunting season, Billy promises the dogs that if they tree a coon, he will do the rest. They tree one in a huge sycamore, which Billy believes is far too large to chop down. Remembering his promise to his dogs, Billy spends the next two days attempting to chop down the sycamore. Exhausted, Billy prays for the strength to continue, whereupon a strong wind blows the tree over.
+      
+      Billy and his hounds become well-known as the best hunters in the Ozarks. Billy's grandfather makes a bet with Rubin and Rainie Pritchard that Old Dan and Little Ann can tree the legendary "ghost coon" that has eluded hunters for years. After a long, complicated hunt, Old Dan and Little Ann manage to tree the raccoon, but having seen how old and smart the ghost coon is, Billy cannot bring himself to kill it. Billy tries to stop the Pritchards from killing the raccoon, leading to a fight with Rubin. The Pritchards' dog Old Blue joins the fight, provoking Old Dan and Little Ann to attack Old Blue to drag him away from Billy. Rubin tries to drive Billy's dogs away with an axe, but trips, falls on the blade, and dies. Billy is deeply troubled by the tragic turn of events, but does not regret his choice to spare the ghost coon.
+      
+      Billy's grandfather enters him into a championship coon hunt against experienced hunters. The hunt is scheduled during a particularly cold week and many of the other hunters are forced to give up, but Billy, who is used to mountain winters, is able to reach the final round. On the last night, Old Dan and Little Ann trap three raccoons in a single tree, but a sudden blizzard forces Billy to take shelter. The following morning, the dogs are found covered in ice but still circling the tree. All three raccoons are captured and Billy and his dogs win the championship and a $300 prize.
+      
+      One night while the trio is hunting, a mountain lion attacks the dogs. Billy fights to save his dogs, but the mountain lion turns on him. The dogs manage to save Billy by killing the mountain lion, but Old Dan later dies of his injuries. Over the next few days, Little Ann loses the will to live and finally dies of grief atop Old Dan's grave, leaving Billy heartbroken.
+      
+      Billy's father tries to comfort his son by explaining that he and Billy's mother have long wished to move to town where their children can get an education, but could not afford to do so without the extra money brought in by Billy's hunting. Knowing that Billy's dogs would suffer in town and that Billy would be devastated to leave them behind, they intended to allow Billy to live with his grandfather. Billy's father believes that God took the dogs as a sign that the family was meant to stay together.
+      
+      On his last day in the Ozarks, Billy visits Old Dan and Little Ann's graves and finds a giant red fern growing between them. Remembering a legend that only an angel can plant a red fern, Billy also comes to believe that perhaps there truly was a higher power at work.
+      
+      The adult Billy closes by saying that although he hasn't returned to the Ozarks, he still dreams of visiting his dogs' graves and seeing the red fern again one day.`
   },
 
   {
