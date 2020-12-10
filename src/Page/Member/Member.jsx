02c94e4eca4e14277e8ec38103a8ac55f7b71952
ctgilbertson <ctgilbertson@wisc.edu>
2020-12-10 15:35:38 -0600

    calendar

--- src/Page/Member/Member.jsx
+++ src/Page/Member/Member.jsx
@@ -12,22 +12,75 @@ import AddToCalendarHOC from 'react-add-to-calendar-hoc';
 import { Row, Button } from "react-bootstrap";
 //var AddToCalendarHOC = require('react-add-to-calendar-hoc');
 
-const startDatetime = moment().utc().add(2, 'days');
-const endDatetime = startDatetime.clone().add(2, 'hours');
-const duration = moment.duration(endDatetime.diff(startDatetime)).asHours();
-const event = {
-  description: 'Description of event. Going to have a lot of fun doing things that we scheduled ahead of time.',
-  duration,
-  endDatetime: endDatetime.format('YYYYMMDDTHHmmssZ'),
-  location: 'NYC',
-  startDatetime: startDatetime.format('YYYYMMDDTHHmmssZ'),
-  title: 'Super Fun Event',
-}
-
-const AddToCalendarModal = AddToCalendarHOC(Button, CalendarModal);
-
 
 export default function Member() {
+  const startDatetime = moment().utc().add(2, 'days');
+  const endDatetime = startDatetime.clone().add(2, 'hours');
+  const duration = moment.duration(endDatetime.diff(startDatetime)).asHours();
+  
+  var gapi = window.gapi
+  var CLIENT_ID = "991246167711-30o5noj5nerk5vav5g1d890tflr0k42u.apps.googleusercontent.com"
+  var API_KEY = "AIzaSyBfho_qWgOQY9gGL2WtfCu7G7r6EhF6Sss"
+  var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+  var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+  
+  const handleClick = () => {
+    gapi.load('client:auth2', () => {
+      console.log('loaded client')
+
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      })
+
+      gapi.client.load('calendar', 'v3', () => console.log('bam!'))
+
+      gapi.auth2.getAuthInstance().signIn()
+      .then(() => {
+        
+        var event = {
+          'summary': 'Awesome Event!',
+          'location': '800 Howard St., San Francisco, CA 94103',
+          'description': 'Really great refreshments',
+          'start': {
+            'dateTime': '2020-06-28T09:00:00-07:00',
+            'timeZone': 'America/Los_Angeles'
+          },
+          'end': {
+            'dateTime': '2020-06-28T17:00:00-07:00',
+            'timeZone': 'America/Los_Angeles'
+          },
+          'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
+          ],
+          'attendees': [
+            {'email': 'lpage@example.com'},
+            {'email': 'sbrin@example.com'}
+          ],
+          'reminders': {
+            'useDefault': false,
+            'overrides': [
+              {'method': 'email', 'minutes': 24 * 60},
+              {'method': 'popup', 'minutes': 10}
+            ]
+          }
+        }
+
+        var request = gapi.client.calendar.events.insert({
+          'calendarId': 'primary',
+          'resource': event,
+        })
+
+        request.execute(event => {
+          console.log(event)
+          window.open(event.htmlLink)
+        })
+      })
+    })
+  }
+
   return (
   <Layout>
     <Container className="MemberContainer">
@@ -48,13 +101,8 @@ export default function Member() {
         </h4>
       </Row>
         <h5 component="p">Thursday at 4pm</h5>
-        <AddToCalendarModal
-            className="farts"
-            linkProps={{
-            className: "farts",
-            }}
-            event={event}
-        />
+      <button onClick={handleClick}>Add Event</button>
+
       <Row>
         <h4 variant="h5" component="h3">
           Tech Team Contact
