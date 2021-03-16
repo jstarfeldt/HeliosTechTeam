2cf28cbb824fc752b34cb581c456beec1d5f5311
Sarah Worthington <sarahworthington@Sarahs-MacBook-Pro-2.local>
2021-03-16 18:04:55 -0500

    teams descriptions

--- src/Page/Member/Member.jsx
+++ src/Page/Member/Member.jsx
@@ -87,9 +87,10 @@ export default function Member() {
           </Row>
           <hr></hr>
             <Image className="m-auto" fluid src={require("../../static/images/engineeringTeam.JPG")}/> 
-            <p className="lead my-1"> The Helios Tech Team is focused on Computer Science and coding. 
-                                      For the Fall 2020 semester we redesigned the Helios website to 
-                                      run on React.js and bootstrap.  </p>
+            <p className="lead my-1"> The engineering team within Helios works on projects with the 
+            university that aim to increase the use of renewable energy (primarily solar) on campus. We 
+            believe the projects we engage in help move us towards a clean energy economy and improve the 
+            campus experience for all.</p>
           </div>
 	      </Tab>
         <Tab eventKey="Energy" title="Energy" style={{backgroundColor: 'white'}}>
@@ -117,9 +118,12 @@ export default function Member() {
           </Row>
           <hr></hr>
             <Image className="m-auto" fluid src={require("../../static/images/techteam.jpg")}/> 
-            <p className="lead my-1"> The Helios Tech Team is focused on Computer Science and coding. 
-                                      For the Fall 2020 semester we redesigned the Helios website to 
-                                      run on React.js and bootstrap.  </p>
+            <p className="lead my-1"> The energy team works to make tangible sustainable change at UW Madison. 
+            We identify opportunities for emissions reductions and have conducted projects like solar installations 
+            and efficient lighting. We get to work closely with UW administrators to determine goals, policy restrictions, 
+            and grant availability. We also work with engineering firms to determine costs and emissions. Any major and 
+            expensive level is welcome, and we would love you to join us and get experience in the sustainability, policy, 
+            and contract work! Check out our panels on top of Gordon’s from the Nick.</p>
           </div>
 	      </Tab>
         <Tab eventKey="Business" title="Business" style={{backgroundColor: 'white'}}>
