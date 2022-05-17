8fca02e0a2ebc1b3b5d8cef2de88be26a44e0f5b
ameyabaxi <102268376+ameyabaxi@users.noreply.github.com>
2022-05-17 17:32:23 -0500

    Outreach + Business Updates

--- src/Page/Member/Member.jsx
+++ src/Page/Member/Member.jsx
@@ -29,8 +29,8 @@ export default function Member() {
   return (
   <Layout>
     <Container className="MemberContainer">
-	    <Tabs defaultActiveKey="Tech" id="uncontrolled-tab-example" className ="team-tabs">
-	      <Tab eventKey="Tech" title="Tech" style={{backgroundColor: 'white'}}>
+	    <Tabs defaultActiveKey="Engineering" id="uncontrolled-tab-example" className ="team-tabs">
+	      /*<Tab eventKey="Tech" title="Tech" style={{backgroundColor: 'white'}}>
           <div class="text-center">
             <h1 class="display-4 font-italic">Technology Team</h1>
           <Row>
@@ -59,7 +59,7 @@ export default function Member() {
                                       For the Fall 2020 semester we redesigned the Helios website to 
                                       run on React.js and bootstrap.  </p>
           </div>
-	      </Tab>
+	      </Tab>*/
         <Tab eventKey="Engineering" title="Engineering" style={{backgroundColor: 'white'}}>
           <div class="text-center">
             <h1 class="display-4 font-italic">Engineering Team</h1>
@@ -133,7 +133,7 @@ export default function Member() {
           <Row>
             <Col>
               <h4 class="h4 bold">Meetings</h4>
-              <h6 class="h5 bold">Tuesdays: 6-7PM</h6>
+              <h6 class="h5 bold">TBA</h6>
               <br/>
               <AddToCalendar event={event}></AddToCalendar>
             </Col>
@@ -142,23 +142,49 @@ export default function Member() {
             </Col>
             <Col style={{textAlign: 'right'}}>
               <p class="h4 bold">Business Team Contact:</p>
-              <p class="h5 bold">Ryan Wenzel</p>
+              <p class="h5 bold">Elizabeth Fields</p>
                 <ul>
-                  <p class="h6 bold">Year: Senior</p>
-                  <p class="h6 bold">Major: Computer Science</p>
-                  <p class="h6 bold">Email: rtwenzel@wisc.edu</p>
+                  <p class="h6 bold">Year: Sophomore</p>
+                  <p class="h6 bold">Major: </p>
+                  <p class="h6 bold">Email: @wisc.edu</p>
                 </ul> 
             </Col>
           </Row>
           <hr></hr>
-            <Image className="m-auto" fluid src={require("../../static/images/techteam.jpg")}/> 
+            /*<Image className="m-auto" fluid src={require("../../static/images/techteam.jpg")}/>*/
 
             <p className="lead my-1"> 
-            The engineering team within Helios works on projects with the university that aim 
-            to increase the use of renewable energy (primarily solar) on campus. We believe 
-            the projects we engage in help move us towards a clean energy economy and improve 
-            the campus experience for all.
+            /*The business team...*/
               </p>
+          </div>
+	      </Tab>
+        <Tab eventKey="Outreach" title="Outreach" style={{backgroundColor: 'white'}}>
+          <div class="text-center">
+            <h1 class="display-4 font-italic">Outreach Team</h1>
+          <Row>
+            <Col>
+              <h4 class="h4 bold">Meetings</h4>
+              <h6 class="h5 bold">TBA</h6>
+              <br/>
+              <AddToCalendar event={event}></AddToCalendar>
+            </Col>
+            <Col style={{textAlign: 'right'}}>
+              <Image className="d-block center" style={{height: '140px', width: '140px'}} src={require("../../static/images/simon.jpg")} roundedCircle />
+            </Col>
+            <Col style={{textAlign: 'right'}}>
+              <p class="h4 bold">Outreach Team Contact:</p>
+              <p class="h5 bold">Ameya Baxi</p>
+                <ul>
+                  <p class="h6 bold">Year: Sophomore</p>
+                  <p class="h6 bold">Major: Environmental Sciences</p>
+                  <p class="h6 bold">Email: abaxi@wisc.edu</p>
+                </ul> 
+            </Col>
+          </Row>
+          <hr></hr>
+            /*<Image className="m-auto" fluid src={require("../../static/images/engineeringTeam.jpg")}/>*/
+            <p className="lead my-1"> The Outreach Team shares with the community projects accomplished by Helios and solar and sustainability news
+	    and knowledge through the website, videos, articles, and events. </p>
           </div>
 	      </Tab>
 	 </Tabs>
